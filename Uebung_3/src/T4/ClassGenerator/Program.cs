9fa59f467193f741f76f2dec28d68fb0e853a592
Thomas Herzog <herzog.thomas81@gmail.com>
2017-04-30 13:45:33 +0200

    - Finished C# part of hands on

--- Uebung_3/src/T4/ClassGenerator/Program.cs
+++ Uebung_3/src/T4/ClassGenerator/Program.cs
@@ -1,8 +1,83 @@
 ﻿using CloningGenerator;
 using System;
+using Persons;
+using Shape;
 
 namespace ClassGenerator {
-  class Program {
-    static void Main(string[] args) {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            testPersons();
+            testShapes();
+
+            Console.Read();
+        }
+
+        public static void testPersons()
+        {
+            Console.WriteLine("################################################################ \n" 
+                            + "testPersons()\n"
+                            + "################################################################");
+
+            Department department = new Department()
+            {
+                Name = "Software engineering"
+            };
+
+            Teacher teacher = new Teacher()
+            {
+                Age = 50,
+                Department = department,
+                Id = "P00000001",
+                Name = "Thomas"
+            };
+
+            Student student = new Student() { 
+            Id = "S1610454013"
+            };
+
+            Console.WriteLine(department + "\n");
+            Console.WriteLine(teacher + "\n");
+            Console.WriteLine(student + "\n");
+
+            Console.WriteLine("################################################################\n");
+        }
+
+        public static void testShapes()
+        {
+            Console.WriteLine("################################################################ \n"
+                              + "testShapes()\n"
+                              + "################################################################");
+
+            Coordinate coordinate = new Coordinate();
+            coordinate.XCoordinate = 1.0;
+            coordinate.YCoordinate = 2.0;
+
+            Rectangular rectangular = new Rectangular();
+            rectangular.Height = 1.0;
+            rectangular.Width = 2.0;
+            rectangular.Name = "Rectangular";
+            rectangular.Coordinate = coordinate;
+            rectangular.FillColor = "#ffffff";
+            rectangular.StrokeHexColor = "#000000";
+            rectangular.Stroke = 1;
+
+            Circle circle = new Circle()
+            {
+                Coordinate = coordinate,
+                FillColor = "#ffffff",
+                Name = "Circle",
+                Radius = 2.0,
+                Stroke = 1,
+                StrokeHexColor = "#000000"
+            };
+
+            Console.WriteLine(coordinate + "\n");
+            Console.WriteLine(rectangular + "\n");
+            Console.WriteLine(circle + "\n");
+
+            Console.WriteLine("################################################################\n");
+        }
     }
 }
--- Uebung_3/src/T4/CloningGenerator/Program.cs
+++ Uebung_3/src/T4/CloningGenerator/Program.cs
@@ -15,19 +15,37 @@ namespace ClassGenerator
             };
 
             a.b = b;
+
+            Console.WriteLine("################################################################ \n"
+                              + "Before clone\n"
+                              + "################################################################");
+            Console.WriteLine("a: " + a.i);
+            Console.WriteLine("a: " + a.i);
+            Console.WriteLine("a: " + a.b.s);
+            Console.WriteLine("a: " + a.b.arr[0]);
+            Console.WriteLine("################################################################\n");
+            
+            // clone object
             var clone = (A)a.Clone();
 
-            Console.WriteLine(a.i);
-            Console.WriteLine(a.b.s);
-            Console.WriteLine(a.b.arr[0]);
-
+            // change value
             a.i = 1337;
             a.b.s = "bye world";
             a.b.arr[0] = 0;
 
-            Console.WriteLine(clone.i);
-            Console.WriteLine(clone.b.s);
-            Console.WriteLine(clone.b.arr[0]);
+            Console.WriteLine("################################################################ \n"
+                              + "After clone\n"
+                              + "################################################################");
+            Console.WriteLine("a: " + a.i);
+            Console.WriteLine("a: " + a.i);
+            Console.WriteLine("a: " + a.b.s);
+            Console.WriteLine("a: " + a.b.arr[0] + "\n");
+
+            Console.WriteLine("clone: " + clone.i);
+            Console.WriteLine("clone: " + clone.i);
+            Console.WriteLine("clone: " + clone.b.s);
+            Console.WriteLine("clone: " + clone.b.arr[0]);
+            Console.WriteLine("################################################################\n");
 
             Console.ReadLine();
         }
